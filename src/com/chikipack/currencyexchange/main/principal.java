087b418c86e69acb89a7cc9e6b1e72ac56cd4691
chikipack <victorgotzilla@hotmail.com>
2024-04-22 18:10:56 -0700

    main

---
+++ src/com/chikipack/currencyexchange/main/principal.java
@@ -0,0 +1,13 @@
+package com.chikipack.currencyexchange.main;
+
+public class principal {
+    public static void main(String[] args) {
+        int salir = 1;
+        while(salir != 0) {
+            System.out.println("***********************");
+            System.out.println("Bienvenido al conversor de moneda");
+            System.out.println("Selecciona un tipo de cambio ");
+
+        }
+    }
+}
